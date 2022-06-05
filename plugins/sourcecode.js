26c4d0a6aaa6b965696091ce2c128ed00c61635e
OneBotz Owner <96653161+davidpangrib001@users.noreply.github.com>
2022-06-05 11:44:18 +0700

    Update sourcecode.js

--- plugins/sourcecode.js
+++ plugins/sourcecode.js
@@ -1,26 +1,24 @@
+let util = require('util')
+let path = require('path')
+let fetch = require('node-fetch')
 let handler = async (m, { conn }) => {
-	
-	// Link Script jgn di ganti ya pliss
-    // Kalo mau masukin github kamu tinggal add aja link githubnya tapi jangan di hapus sumber script!
-    // Jangan Ngeyell..
-    // Kalo ngeyel gk bakal gw up sc lgi.
-    
-    let txt = `
-Bot ini menggunakan script github
-
-https://github.com/FokusDotId/Family-MD.git
-
-pliss follow dan kasih ✨⭐⭐🌟 ya pak!
-
-My Github: https://github.com/FokusDotId
-
-`
-     conn.reply(m.chat, txt, m)
+let vn = './sc.mp3'
+conn.sendFile(m.chat, vn, 'sc.mp3', null, m, true, {
+type: 'audioMessage', // paksa tanpa convert di ffmpeg
+ptt: true, contextInfo: { 
+         externalAdReply: { 
+             title: 'Nih Bang Scriptnya..',  
+             body: 'Semoga Bermanfaat 🗿👍', 
+             description: '', 
+             mediaType: 2, 
+           thumbnail: await (await fetch('https://i.pinimg.com/originals/66/30/37/663037f00539f3bc1dbd3efeae0d700b.jpg')).buffer(), 
+          mediaUrl: `https://youtu.be/35w7z9QFLwY` 
+         } 
+      } 
+   })
 }
 handler.help = ['sourcecode']
 handler.tags = ['info']
-handler.command = /^(sc(ript(bot)?)?|sourcecode)$/i
+handler.command = /^(sourcecode|sc|scbot|script|github)$/i
 
 module.exports = handler
-
-
